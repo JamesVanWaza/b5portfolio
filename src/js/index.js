9463f447c3fac0d104f3eed4f303e98a5573a46f
JamesVanWaza <jgacuca@gmail.com>
2023-09-27 08:29:01 -0400

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,45 @@
+/** Bootstrap JS */
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
+
+/** PopperJS */
+import '@popperjs/core';
+
+/** Botstrap SCSS **/
+import '../scss/style.scss';
+
+/** FontAwesome 6 */
+import '@fortawesome/fontawesome-free/js/all.js';
+
+/** Footer */
+document.body.onload = footer;
+
+function footer() {
+    // create a new div element
+    const footerDiv = document.createElement("footer");
+
+    // assign it a class
+    footerDiv.classList.add("footer");
+    footerDiv.classList.add("mt-auto");
+    footerDiv.classList.add("text-center");
+    footerDiv.classList.add("py-3");
+
+
+    // gets the current date
+    const copyright = new Date().getFullYear();
+
+    // gets the copyright symbol
+    const favicon = document.createElement("i");
+    favicon.classList.add("fas", "fa-copyright");
+
+    const text = document.createTextNode(" 2020 " + "- " + copyright);
+
+    // add the text node to the newly created div
+    footerDiv.appendChild(favicon);
+    footerDiv.appendChild(text);
+
+    // add the newly created element and its content into the DOM
+    const newDiv = document.getElementById("div");
+    document.body.insertBefore(footerDiv, newDiv);
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const TerserPlugin = require("terser-webpack-plugin");
+
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+module.exports = {
+    //	https://webpack.js.org/configuration/mode/
+    mode: 'development',
+    entry: "./src/js/index.js",
+    output: {
+        //		filename: "[name].bundle.js",
+        filename: "main.js",
+        //		path: path.resolve(__dirname, "public") Can change directory name
+        path: path.resolve(__dirname, "public"),
+        // For the images to be loaded in the browser
+        assetModuleFilename: 'assets/resource/[hash][ext][query]'
+    },
+    target: "web",
+    devServer: {
+        hot: true,
+        watchFiles: ['src/'],
+        open: {
+            app: 'Google Chrome'
+        },
+        static: {
+            directory: path.join(__dirname, 'public'),
+            watch: true,
+        },
+        port: 9004
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()]
+    },
+    plugins: [
+        // Main Page
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/html-templates/index-template.html'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'theming.html',
+            template: './src/html-templates/theming-template.html'
+        }),
+        // Favicon
+        new FaviconsWebpackPlugin({
+            logo: './src/images/B5P-Color@4x.png'
+        })
+    ],
+    module: {
+        rules: [{
+            // Whenever a javascript file is found, babel should run and do not compile node_module files
+            test: /\js$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+                // Creates `style` nodes from JS Strings
+                { loader: 'style-loader' },
+
+                // Translates CSS into CommonJS
+                { loader: 'css-loader' },
+
+                // Compiles Sass to CSS
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        implementation: require('sass')
+                    }
+                },
+            ]
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+        },
+        {
+            test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+            type: 'asset/resource',
+        }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss']
+    }
+};
