567123056cb0c064b0ba7ec64adcc2140863d738
JamesVanWaza <jgacuca@gmail.com>
2023-11-13 10:19:20 -0500

    WIP on adding lightbox.js

---
+++ src/js/lightbox.js
@@ -0,0 +1,10 @@
+import Glightbox from 'glightbox';
+
+const lightbox = Glightbox(
+    {
+        'href': 'https://www.youtube.com/watch?v=Ga6RYejo6Hk',
+        'type': 'video',
+        'source': 'youtube', //vimeo, youtube or local
+        'width': 300,
+    }
+);
