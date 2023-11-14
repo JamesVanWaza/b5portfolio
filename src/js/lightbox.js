6a363c122b9d51961788f9876db0ec1c6dd1fc1f
JamesVanWaza <jgacuca@gmail.com>
2023-11-14 09:43:58 -0500

    Setting up the Lightbox JS

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -5,6 +5,6 @@ const lightbox = Glightbox(
         'href': 'https://www.youtube.com/watch?v=Ga6RYejo6Hk',
         'type': 'video',
         'source': 'youtube', //vimeo, youtube or local
-        'width': 300,
+        'width': 500,
     }
 );
